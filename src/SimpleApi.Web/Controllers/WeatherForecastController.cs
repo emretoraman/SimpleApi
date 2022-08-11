85c3eecfe88fb24b2ca5d39779334ca4348f50df
Emre Toraman <emretoraman@gmail.com>
2022-08-11 22:02:40 +0300

    Fixed unit tests :)

--- src/SimpleApi.Web/Controllers/WeatherForecastController.cs
+++ src/SimpleApi.Web/Controllers/WeatherForecastController.cs
@@ -14,14 +14,13 @@ namespace SimpleApi.Web.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast>? Get()
         {
-            return null;
-            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            //{
-            //    Date = DateTime.Now.AddDays(index),
-            //    TemperatureC = Random.Shared.Next(-20, 55),
-            //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            //})
-            //.ToArray();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
         }
     }
 }
