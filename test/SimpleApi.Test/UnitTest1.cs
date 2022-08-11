7fe96698f337198f691f18e82ed3c9bae3fb347f
Emre Toraman <emretoraman@gmail.com>
2022-08-11 20:24:46 +0300

    Add project files.

---
+++ test/SimpleApi.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SimpleApi.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ test/SimpleApi.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SimpleApi.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
